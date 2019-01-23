5b962affd5b28deea7d95823aa910334ae481c83
Sergey Chimanyan <chimanyans@gmail.com>
2019-01-23 12:30:30 +0400

    build fix

--- bin/utils.js
+++ bin/utils.js
@@ -1,6 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var bs58_1 = require("bs58");
+var BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
+// var bs58 = require('base-x')(BASE58);
+var basex = require("base-x");
+var bs58 = basex(BASE58_ALPHABET);
+// const BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+// import bs58 from 'base-x';
+// const genBs58 = bs58(BASE58_ALPHABET);
 exports.hexStringToByteArray = function (hexStr) {
     return new Uint8Array(hexStr.match(/[\da-f]{2}/gi).map(function (h) {
         return parseInt(h, 16);
@@ -10,5 +17,7 @@ exports.hexStringToBase58 = function (hexStr) {
     return bs58_1.encode(exports.hexStringToByteArray(hexStr));
 };
 exports.derToBase58 = function (der) {
-    return bs58_1.encode(der);
+    // console.log("1: ", encode(der))
+    // console.log("2: ", bs58.encode(der))
+    return bs58.encode(der); //encode(der);
 };
