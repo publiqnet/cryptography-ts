287b14cf09512a3b820f933372be0d9aed86dd88
Sergey Chimanyan <chimanyans@gmail.com>
2019-01-23 12:29:25 +0400

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,15 @@ import * as RMD160 from 'crypto-js/ripemd160';
 import * as SHA256  from 'crypto-js/sha256';
 import * as CryptoJS from 'crypto-js';
 
+var BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+// var bs58 = require('base-x')(BASE58);
+import * as basex from 'base-x';
+const bs58 = basex(BASE58_ALPHABET)
+
+// const BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+// import bs58 from 'base-x';
+// const genBs58 = bs58(BASE58_ALPHABET);
+
 export const hexStringToByteArray = hexStr => {
     return new Uint8Array(hexStr.match(/[\da-f]{2}/gi).map( h => {
         return parseInt(h, 16);
@@ -15,5 +24,9 @@ export const hexStringToBase58 = hexStr => {
 
 
 export const derToBase58 = der => {
-    return encode(der);
+    // console.log("1: ", encode(der))
+    // console.log("2: ", bs58.encode(der))
+
+
+    return  bs58.encode(der)//encode(der);
 };
