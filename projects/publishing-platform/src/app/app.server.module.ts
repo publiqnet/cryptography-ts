9f5aaa2729eea20212ab03cab0e9033582e769be
Harutyun Mnatsakanyan <harmn90@gmail.com>
2019-03-13 12:50:19 +0400

    lib import las part

--- projects/publishing-platform/src/app/app.server.module.ts
+++ projects/publishing-platform/src/app/app.server.module.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { FlexLayoutServerModule } from '@angular/flex-layout/server';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateUniversalLoader } from 'shared-lib'; // need change
+import { TranslateUniversalLoader } from 'shared-lib';
 
 export function translateFactory() {
   return new TranslateUniversalLoader('./dist/publishing-platform/assets/i18n', '.json');
