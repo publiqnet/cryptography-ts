4ff41910a98650a84b6c6da687bae0cadb0b09a6
haykgrigoryan <haykgrigoryan1989@gmail.com>
2019-03-19 17:07:45 +0400

    fix

--- projects/publishing-platform/src/environments/environment.ts
+++ projects/publishing-platform/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  backend: 'https://api.publiq.network',
-  ds_backend: 'https://ds.publiq.network',
+  backend: 'https://stage-api.publiq.network',
+  ds_backend: 'https://stage-ds.publiq.network',
   daemon_address_first: 'wss://south.publiq.network:443/websocket_stage',
   daemon_address_second: 'wss://south.publiq.network:8092/websocket_stage',
   currentVersion: 'presentation',
