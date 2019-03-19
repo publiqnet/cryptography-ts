02b0f04b6341153e6c3a15aea7570a6a0533aaca
haykgrigoryan <haykgrigoryan1989@gmail.com>
2019-03-19 16:55:50 +0400

    create environments

--- projects/publishing-platform/src/environments/environment.prod.ts
+++ projects/publishing-platform/src/environments/environment.prod.ts
@@ -1,28 +1,15 @@
-import {
-  backend,
-  currentVersion,
-  daemonAddressFirst,
-  daemonAddressSecond,
-  dsBackend,
-  explorerAddress,
-  GA_ID,
-  froalaEditorKey,
-  filestorageLink,
-  daemonHttpsAddress,
-  mainSiteUrl
-} from './parameters';
-
 export const environment = {
   production: true,
-  backend: backend,
-  ds_backend: dsBackend,
-  daemon_address_first: daemonAddressFirst,
-  daemon_address_second: daemonAddressSecond,
-  currentVersion: currentVersion,
-  explorerAddress: explorerAddress,
-  google_analytics_id: GA_ID,
-  froala_editor_key: froalaEditorKey,
-  filestorage_link: filestorageLink,
-  daemon_https_address: daemonHttpsAddress,
-  main_site_url: mainSiteUrl
+  backend: 'https://stage-api.publiq.network',
+  ds_backend: 'https://stage-ds.publiq.network',
+  daemon_address_first: 'wss://south.publiq.network:443/websocket_stage',
+  daemon_address_second: 'wss://south.publiq.network:8092/websocket_stage',
+  currentVersion: 'presentation',
+  explorerAddress: 'https://explorer.publiq.network',
+  google_analytics_id: 'NUA-99830524-3',
+  froala_editor_key: '1D4I4C10B6eF5C4C3E3E2C2A5D6A3A1xusedc1D-17C6F-11dzj==',
+  filestorage_link: 'https://stage-file.publiq.network',
+  daemon_https_address: 'https://south.publiq.network:443/api_stage',
+  main_site_url: 'https://stage.publiq.site'
 };
+
