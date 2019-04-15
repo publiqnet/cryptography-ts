d8583930486665c58590a034fe37820b09ff4d39
Harutyun Mnatsakanyan <harmn90@gmail.com>
2019-04-15 18:07:13 +0400

    blockchain link changes

--- projects/wallet-app/src/environments/environment.prod.ts
+++ projects/wallet-app/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   backend: 'https://stage-mainnet-wallet-api.publiq.network',
   oauth_backend: 'https://stage-mainnet-oauth.publiq.network',
   explorerAddress: 'https://explorer.publiq.network',
-  blockchain_api_url: 'https://north.publiq.network/publiq/api',
+  blockchain_api_url: 'http://north.publiq.network/testnet_publiq/api',
   coinName: 'PBQ',
   auto_logout_time: 300
 };
