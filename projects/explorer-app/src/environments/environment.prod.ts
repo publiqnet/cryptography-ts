dffae4a4fe69f5769e748b56ec6f7133c74ba38d
Harutyun Mnatsakanyan <harmn90@gmail.com>
2019-04-20 17:17:49 +0400

    prod domain changes

--- projects/explorer-app/src/environments/environment.prod.ts
+++ projects/explorer-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backend: 'https://mainnet-wallet-api.publiq.network'
+  backend: 'https://wallet-api.publiq.network'
 };
--- projects/wallet-app/src/environments/environment.prod.ts
+++ projects/wallet-app/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  backend: 'https://mainnet-wallet-api.publiq.network',
-  oauth_backend: 'https://mainnet-oauth.publiq.network',
+  backend: 'https://wallet-api.publiq.network',
+  oauth_backend: 'https://oauth.publiq.network',
   explorerAddress: 'https://explorer.publiq.network',
-  blockchain_api_url: 'https://north.publiq.network/testnet_publiq/api',
+  blockchain_api_url: 'https://north.publiq.network/publiq/api',
   coinName: 'PBQ',
   auto_logout_time: 300
 };
