30a1d77d1f0a92d9603d39ea28c96e62905570bb
Harutyun Mnatsakanyan <harmn90@gmail.com>
2019-04-22 17:00:37 +0400

    Block title changes

--- projects/explorer-app/src/app/homepage/homepage.component.ts
+++ projects/explorer-app/src/app/homepage/homepage.component.ts
@@ -7,8 +7,8 @@ import { ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-homepage',
-  templateUrl: 'homepage.component.html',
-  styleUrls: []
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit, OnDestroy {
   today: Date = new Date();
