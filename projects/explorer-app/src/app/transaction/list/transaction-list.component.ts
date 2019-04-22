6cb7588ceb7fad51ab7fe07162dd606848ee8aee
Harutyun Mnatsakanyan <harmn90@gmail.com>
2019-04-22 16:46:23 +0400

    transactions list changes + put in homepage

--- projects/explorer-app/src/app/transaction/list/transaction-list.component.ts
+++ projects/explorer-app/src/app/transaction/list/transaction-list.component.ts
@@ -1,46 +1,57 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, Inject, Input, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Transaction } from '../../services/models/Transaction';
 import { ApiService } from '../../services/api.service';
 import { filter, takeUntil } from 'rxjs/operators';
 import { ReplaySubject } from 'rxjs';
 import { TransactionResponse } from '../../services/models/TransactionResponse';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-transaction-list',
   templateUrl: './transaction-list.component.html',
+  styleUrls: ['./transaction-list.component.scss'],
   styles: []
 })
 export class TransactionListComponent implements OnInit, OnDestroy {
+
+  @Input() loadOnScroll = true;
+  @Input() transactionsLimit = 10;
 
   transactions: Transaction[];
   blockInfiniteScroll = false;
   seeMoreChecker = false;
   lastTransactionHash = '';
   loadingBlocks = true;
-  transactionsLimit = 10;
   transactionsFrom = 0;
   hasBeenLoaded: boolean;
   private unsubscribe$ = new ReplaySubject<void>(1);
 
-  constructor(private route: ActivatedRoute, private apiService: ApiService) {
+  constructor(private route: ActivatedRoute,
+              private apiService: ApiService,
+              private router: Router,
+              @Inject(PLATFORM_ID) private platformId: Object
+              ) {
   }
 
   ngOnInit() {
-    this.apiService.getTransactions(null, this.transactionsLimit)
-      .pipe(
-        filter((data: any) => data != null),
-        takeUntil(this.unsubscribe$)
-      )
-      .subscribe((data: TransactionResponse) => {
-        this.seeMoreChecker = data.more;
-        this.transactionsData = data.transactions;
-      });
+    if (isPlatformBrowser(this.platformId)) {
+      this.apiService.getTransactions(null, this.transactionsLimit)
+        .pipe(
+          filter((data: any) => data != null),
+          takeUntil(this.unsubscribe$)
+        )
+        .subscribe((data: TransactionResponse) => {
+          this.seeMoreChecker = data.more;
+          this.transactionsData = data.transactions;
+        });
+    }
   }
 
   set transactionsData(transactions: Transaction[]) {
     this.transactions = transactions;
     this.calculateLastTransactionHash(this.transactions);
+    this.loadingBlocks = false;
   }
 
   seeMore() {
@@ -70,6 +81,12 @@ export class TransactionListComponent implements OnInit, OnDestroy {
         this.lastTransactionHash = transactions[last].transactionHash;
       }
     }
+  }
+
+  redirect($event, page, param) {
+    $event.preventDefault();
+    this.transactions = null;
+    this.router.navigate([`/${page}/${param}`]);
   }
 
   ngOnDestroy(): void {
