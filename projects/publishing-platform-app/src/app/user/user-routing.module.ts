1bfcd84ebaa7de32a8fb2d1e23376772439bfadc
garikarakelyan <garik.arakelyan92@gmail.com>
2019-07-18 15:11:44 +0400

    working on registration page

--- projects/publishing-platform-app/src/app/user/user-routing.module.ts
+++ projects/publishing-platform-app/src/app/user/user-routing.module.ts
@@ -4,6 +4,7 @@ import { UserTemplateComponent } from './user-template/user-template.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RecoverComponent } from './recover/recover.component';
+import { RegistrationPasswordComponent } from './registration-password/registration-password.component';
 
 export const userRoutes: Routes = [
   {
@@ -24,7 +25,15 @@ export const userRoutes: Routes = [
         path: 'recover',
         pathMatch: 'full',
         component: RecoverComponent
-      }
+      },
+      {
+        path: 'signup/confirmation/:code',
+        data: {
+          action: 'signup'
+        },
+        pathMatch: 'full',
+        component: RegistrationPasswordComponent
+      },
     ]
   }
 ];
