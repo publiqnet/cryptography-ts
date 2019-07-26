904d3a3aa89e09e8cb7f788dc518544c0cb8c3e7
Gohar Avetisyan <gohar.avetisyan10@gmail.com>
2019-07-26 18:01:09 +0400

    changes

--- projects/publishing-platform-app/src/app/core/homepage/homepage.component.ts
+++ projects/publishing-platform-app/src/app/core/homepage/homepage.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgxMasonryOptions } from 'ngx-masonry';
+import { ContentService } from '../services/content.service';
+import { Content } from '../services/models/content';
 
 @Component({
   selector: 'app-homepage',
@@ -12,6 +14,12 @@ export class HomepageComponent implements OnInit, OnDestroy {
   public contentArray = [];
   public isMasonryLoaded = false;
   public listType = 'grid';
+  public seeMoreChecker = false;
+  seeMoreLoading = false;
+  public blockInfiniteScroll = false;
+  requestMade = false;
+  private storiesDefaultCount = 30;
+  private firstBlock = 30;
 
   public myOptions: NgxMasonryOptions = {
     transitionDuration: '0s',
@@ -84,66 +92,87 @@ export class HomepageComponent implements OnInit, OnDestroy {
 
   constructor(
     private router: Router,
+    private contentService: ContentService
   ) {
 
   }
 
   ngOnInit() {
-    for (let i = 0; i < 30; ++i) {
-      const sentences = [
-        'so fat not even Dora can explore her',
-        'so  fat I swerved to miss her and ran out of gas',
-        'so smelly she put on Right Guard and it went left',
-        'so fat she hasn’t got cellulite, she’s got celluheavyso  fat I swerved to miss her and ran out of gasso  ' +
-        'fat I swerved to miss her and ran out of gasand ran out of gasso  fat I swerved to miss her and ran out of gasand ran ' +
-        'out of gasso  fat I swerved to miss her and ran out of gas',
-        'so fat she don’t need no internet – she’s already world wide',
-        'so hair her armpits look like Don King in a headlockso  fat I swerved to miss her and ran out of gas',
-        'so classless she could be a Marxist utopiaso  fat I swerved to miss her and ran out of gasand' +
-        ' ran out of gasso  fat I swerved to miss her and ran out of gasand ran out of gasso  fat I swerved ' +
-        'to miss her and ran out of gasand ran out of gasso  fat I swerved to miss her and ran out of gas',
-        'so fat she can hear bacon cooking in Canada',
-        'so fat she won “The Bachelor” because she all those other bitches',
-        'so stupid she believes everything that Brian Williams says',
-        'so ugly she scared off Flavor Flav',
-        'is like Domino’s Pizza, one call does it alland ran out of gasso  fat I swerved ' +
-        'to miss her and ran out of gasand ran out of gasso  fat I swerved to miss her and ran out of gasand ' +
-        'ran out of gasso  fat I swerved to miss her and ran out of gas',
-        'is twice the man you are',
-        'is like Bazooka Joe, 5 cents a blow',
-        'is like an ATM, open 24/7',
-        'is like a championship ring, everybody puts a finger in her'
-      ];
-      this.contentArray.push({
-        'slug': '5ceb9fc82765246c6cc55b47',
-        'author': {
-          'slug': '1.0.2',
-          'first_name': 'Gohar',
-          'last_name': 'Avetisyan',
-          'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzlDPRr1xSW0lukY2EmVpAx5Ye1S8H5luUVOK2IqFdcsjCDQxK'
-        },
-        'created': '11 dec 2019',
-        'published': '12 dec 2019',
-        'title': 'In the flesh: translating 2d scans into 3d prints NO' + (i + 1) + (sentences[i] ? sentences[i] : '') ,
-        'tags': [
-          '2017',
-          'DEVELOPER',
-          'FULLSTACK'
-        ],
-        'image': 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        'publication': {
-          'title': 'UX Planet',
-          'slug': 'ux_planet'
-        },
-        'view_count': '1K'
-      });
-    }
+    // for (let i = 0; i < 30; ++i) {
+    //   const sentences = [
+    //     'so fat not even Dora can explore her',
+    //     'so  fat I swerved to miss her and ran out of gas',
+    //     'so smelly she put on Right Guard and it went left',
+    //     'so fat she hasn’t got cellulite, she’s got celluheavyso  fat I swerved to miss her and ran out of gasso  ' +
+    //     'fat I swerved to miss her and ran out of gasand ran out of gasso  fat I swerved to miss her and ran out of gasand ran ' +
+    //     'out of gasso  fat I swerved to miss her and ran out of gas',
+    //     'so fat she don’t need no internet – she’s already world wide',
+    //     'so hair her armpits look like Don King in a headlockso  fat I swerved to miss her and ran out of gas',
+    //     'so classless she could be a Marxist utopiaso  fat I swerved to miss her and ran out of gasand' +
+    //     ' ran out of gasso  fat I swerved to miss her and ran out of gasand ran out of gasso  fat I swerved ' +
+    //     'to miss her and ran out of gasand ran out of gasso  fat I swerved to miss her and ran out of gas',
+    //     'so fat she can hear bacon cooking in Canada',
+    //     'so fat she won “The Bachelor” because she all those other bitches',
+    //     'so stupid she believes everything that Brian Williams says',
+    //     'so ugly she scared off Flavor Flav',
+    //     'is like Domino’s Pizza, one call does it alland ran out of gasso  fat I swerved ' +
+    //     'to miss her and ran out of gasand ran out of gasso  fat I swerved to miss her and ran out of gasand ' +
+    //     'ran out of gasso  fat I swerved to miss her and ran out of gas',
+    //     'is twice the man you are',
+    //     'is like Bazooka Joe, 5 cents a blow',
+    //     'is like an ATM, open 24/7',
+    //     'is like a championship ring, everybody puts a finger in her'
+    //   ];
+    //   this.contentArray.push({
+    //     'slug': '5ceb9fc82765246c6cc55b47',
+    //     'author': {
+    //       'slug': '1.0.2',
+    //       'first_name': 'Gohar',
+    //       'last_name': 'Avetisyan',
+    //       'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzlDPRr1xSW0lukY2EmVpAx5Ye1S8H5luUVOK2IqFdcsjCDQxK'
+    //     },
+    //     'created': '11 dec 2019',
+    //     'published': '12 dec 2019',
+    //     'title': 'In the flesh: translating 2d scans into 3d prints NO' + (i + 1) + (sentences[i] ? sentences[i] : ''),
+    //     'tags': [
+    //       '2017',
+    //       'DEVELOPER',
+    //       'FULLSTACK'
+    //     ],
+    //     'image': 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    //     'publication': {
+    //       'title': 'UX Planet',
+    //       'slug': 'ux_planet'
+    //     },
+    //     'view_count': '1K'
+    //   });
+    // }
+    this.contentService.getHomePageContents(this.firstBlock, this.storiesDefaultCount)
+    .subscribe((contentData: any) => {
+        console.log(contentData);
+        this.contentArray = contentData.data;
+        this.seeMoreChecker = contentData.more;
+        this.seeMoreLoading = false;
+      }
+    );
+
   }
 
   onLayoutComplete(event) {
     if (event && event.length > 1) {
       this.isMasonryLoaded = true;
     }
+  }
+
+  seeMore() {
+    this.seeMoreLoading = true;
+    this.blockInfiniteScroll = true;
+    this.contentService.getHomePageContents(this.storiesDefaultCount, this.contentArray[this.contentArray.length - 1].uri).subscribe(
+      (data: any) => {
+        this.seeMoreChecker = data.more;
+        this.seeMoreLoading = false;
+      }
+    );
   }
 
   ngOnDestroy() {
