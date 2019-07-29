380407309ad80847534f04ae098b8bd4af4c690f
Harutyun Mnatsakanyan <harmn90@gmail.com>
2019-07-29 20:17:11 +0400

    load homepage

--- projects/publishing-platform-app/src/app/core/homepage/homepage.component.ts
+++ projects/publishing-platform-app/src/app/core/homepage/homepage.component.ts
@@ -18,7 +18,8 @@ export class HomepageComponent implements OnInit, OnDestroy {
   seeMoreLoading = false;
   public blockInfiniteScroll = false;
   requestMade = false;
-  private storiesDefaultCount = 6;
+  public startFromUri = null;
+  public storiesDefaultCount = 2;
   public firstRelevantBlock = [];
   public secondRelevantBlock = [];
   public firstContentBlock = [];
@@ -99,19 +100,30 @@ export class HomepageComponent implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit() {
-    this.contentService.getHomePageContents(null, this.storiesDefaultCount)
+    this.contentService.getHomePageContents(this.startFromUri, this.storiesDefaultCount + 1)
       .subscribe((contentData: any) => {
         this.contentArray = contentData.data;
         this.seeMoreChecker = contentData.more;
         this.seeMoreLoading = false;
-        this.firstRelevantBlock = this.contentArray.slice(0, this.storiesDefaultCount);
-        this.secondRelevantBlock = this.contentArray.slice(0, this.storiesDefaultCount);
-        this.firstContentBlock = this.contentArray.slice(0, this.storiesDefaultCount / 2);
-        this.secondContentBlock = this.contentArray.slice(this.storiesDefaultCount / 2, this.storiesDefaultCount);
-        this.loadedContentBlock = this.contentArray.slice(this.storiesDefaultCount);
-      }
-      );
+        if (this.contentArray.length) {
+          this.calculateLastStoriUri();
+          this.firstRelevantBlock = this.contentArray.slice(0, this.storiesDefaultCount);
+          this.secondRelevantBlock = this.contentArray.slice(0, this.storiesDefaultCount);
+          this.firstContentBlock = this.contentArray.slice(0, this.storiesDefaultCount / 2);
+          this.secondContentBlock = this.contentArray.slice(this.storiesDefaultCount / 2, this.storiesDefaultCount);
+          this.loadedContentBlock = this.contentArray.slice(this.storiesDefaultCount);
+        }
+      });
+  }
 
+  calculateLastStoriUri() {
+    if (this.contentArray.length > this.storiesDefaultCount) {
+      const lastIndex = this.contentArray.length - 1;
+      if (this.contentArray[lastIndex].uri !== this.startFromUri) {
+        this.startFromUri = this.contentArray[lastIndex].uri;
+        this.contentArray.pop();
+      }
+    }
   }
 
   onLayoutComplete(event) {
@@ -123,12 +135,14 @@ export class HomepageComponent implements OnInit, OnDestroy {
   seeMore() {
     this.seeMoreLoading = true;
     this.blockInfiniteScroll = true;
-    this.contentService.getHomePageContents(this.contentArray[this.contentArray.length - 1].uri, this.storiesDefaultCount).subscribe(
+    this.contentService.getHomePageContents(this.startFromUri, this.storiesDefaultCount).subscribe(
       (data: any) => {
         this.seeMoreChecker = data.more;
         this.seeMoreLoading = false;
+        this.calculateLastStoriUri();
         this.contentArray = this.contentArray.concat(data.data);
         this.loadedContentBlock = this.loadedContentBlock.concat(data.data);
+        this.blockInfiniteScroll = false;
       }
     );
   }
