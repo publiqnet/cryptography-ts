8ad8213aa35013700ad67a5b4ca4d4b649666f0a
sarkisandreyan <sarkis.andreyan@gmail.com>
2019-08-06 16:33:59 +0400

    publication list additions

--- projects/publishing-platform-app/src/app/publication/my-publications/my-publications.component.ts
+++ projects/publishing-platform-app/src/app/publication/my-publications/my-publications.component.ts
@@ -3,77 +3,50 @@ import { Router } from '@angular/router';
 import { PublicationService } from '../../core/services/publication.service';
 import { AccountService } from '../../core/services/account.service';
 
+import { Publication } from '../../core/services/models/publication';
+import { Publications } from '../../core/services/models/publications';
+
+import { ReplaySubject } from 'rxjs';
+import { filter, switchMap, takeUntil } from 'rxjs/operators';
+
 @Component({
   selector: 'app-my-publications',
   templateUrl: './my-publications.component.html',
   styleUrls: ['./my-publications.component.scss']
 })
 export class MyPublicationsComponent implements OnInit, OnDestroy {
-  public publications = [];
+  public publications: Publication[];
+  public membership: Publication[];
+  public invitations: Publication[];
+  public requests: Publication[];
+
+  private unsubscribe$ = new ReplaySubject<void>(1);
 
   constructor(
     public router: Router,
     public publicationService: PublicationService,
     private accountService: AccountService
   ) {
-    for (let i = 0; i < 4; ++i) {
-      this.publications.push({
-        'title': 'UX Topics',
-        'description': 'Tips & News on Social Media Marketing, Online Advertising, Search Engine Optimization, Content Marketing, Growth Hacking, Branding, Start-Ups and more.',
-        'logo': 'http://via.placeholder.com/120x120',
-        'cover': 'https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        'slug': 'ux_topics',
-        'subscribers': 1234,
-        'following': false,
-        'inviter': {
-          'name': 'Anechka'
-        },
-        'status': 0,
-        'storiesCount': 0,
-        'membersList': [
-          {
-            'slug': '1.0.2',
-            'first_name': 'test 1',
-            'last_name': 'A',
-            'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzlDPRr1xSW0lukY2EmVpAx5Ye1S8H5luUVOK2IqFdcsjCDQxK'
-          },
-          {
-            'slug': '1.0.2',
-            'first_name': 'test 2',
-            'last_name': 'B',
-            'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzlDPRr1xSW0lukY2EmVpAx5Ye1S8H5luUVOK2IqFdcsjCDQxK'
-          },
-          {
-            'slug': '1.0.2',
-            'first_name': 'test 3',
-            'last_name': 'C',
-            'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzlDPRr1xSW0lukY2EmVpAx5Ye1S8H5luUVOK2IqFdcsjCDQxK'
-          },
-          {
-            'slug': '1.0.2',
-            'first_name': 'test 4',
-            'last_name': 'D',
-            'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzlDPRr1xSW0lukY2EmVpAx5Ye1S8H5luUVOK2IqFdcsjCDQxK'
-          },
-          {
-            'slug': '1.0.2',
-            'first_name': 'test 5',
-            'last_name': 'E',
-            'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzlDPRr1xSW0lukY2EmVpAx5Ye1S8H5luUVOK2IqFdcsjCDQxK'
-          },
-          {
-            'slug': '1.0.2',
-            'first_name': 'test 6',
-            'last_name': 'G',
-            'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzlDPRr1xSW0lukY2EmVpAx5Ye1S8H5luUVOK2IqFdcsjCDQxK'
-          }
-        ]
-      });
-    }
+
+  }
+
+  set PublicationsData(data: Publications) {
+    this.publications = data.owned;
+    this.membership = data.membership;
+    this.invitations = data.invitations;
+    this.requests = data.requests;
   }
 
   ngOnInit() {
+    this.publicationService.getMyPublications()
+      .pipe(
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe((data: Publications) => this.PublicationsData = data);
+  }
 
+  changeRoute(url) {
+    this.router.navigateByUrl(url);
   }
 
   ngOnDestroy() {
